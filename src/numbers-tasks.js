d22fe227bc2c892821e93f3b4d64e0c48c11b81f
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-10 14:36:48 +0400

    feat: solve isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -198,8 +198,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+
+  for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
 }
 
 /**

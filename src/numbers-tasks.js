b605683187365ea4d8900d6d55c9b9d91347c7eb
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-10 14:56:03 +0400

    feat: solve getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -257,8 +257,10 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) return 0;
+  if (index === 1) return 1;
+  return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
 }
 
 /**

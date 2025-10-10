1bcbf5b8097eb8239c7f57bdb560925cc4d051d7
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-10 17:39:12 +0400

    feat: solve roundToNearestInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -528,8 +528,8 @@ function roundToLargestInteger(number) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function roundToNearestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToNearestInteger(number) {
+  return Math.round(number);
 }
 
 /**

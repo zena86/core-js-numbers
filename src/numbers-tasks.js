294a98d9f66fa3dab1dcf62ee03d31d7c9dcca90
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-10 17:45:19 +0400

    feat: solve getSumOfNumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -559,8 +559,8 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  return Number((x1 + x2 + x3).toFixed(12));
 }
 
 /**

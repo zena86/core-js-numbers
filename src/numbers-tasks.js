102a3c549a9484440e2a83008fcac0d983a50c0e
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-10 17:21:48 +0400

    feat: solve toPrecision

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -390,8 +390,8 @@ function toFixed(number, fractionDigits) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  return number.toPrecision(precision);
 }
 
 /**

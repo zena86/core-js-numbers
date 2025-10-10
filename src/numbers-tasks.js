9d600fbfcd38f314267c7449381f550aafdb7d80
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-10 15:04:15 +0400

    feat: solve getSumToN

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -274,8 +274,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i += 1) {
+    sum += i;
+  }
+  return sum;
 }
 
 /**

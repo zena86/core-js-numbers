9092bf801eb924e260a95f5dff5a9f5832b97e9e
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-10 17:54:38 +0400

    feat: solve getHypotenuse

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -605,8 +605,8 @@ function getRandomInteger(min, max) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**

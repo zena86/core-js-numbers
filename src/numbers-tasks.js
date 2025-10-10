2b2d62d2e3430e336d71dafba21196ae8f52178a
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-10 17:33:16 +0400

    feat: solve getIntegerOnString

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -470,8 +470,8 @@ function getFloatOnString(str) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  return Number.parseInt(str, base);
 }
 
 /**
